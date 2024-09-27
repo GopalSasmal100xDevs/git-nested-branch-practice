33983ae8365665511aa6b212e522c9814d1d9046
gopalprojectx100 <gopalsasmal100xdevs@gmail.com>
2024-09-27 10:52:22 +0530

    Feature-3 javascript code

--- index.js
+++ index.js
@@ -58,3 +58,24 @@ console.log(
 console.log(
   invert_key_value({ red: "#FF0000", green: "#00FF00", white: "#FFFFFF" })
 );
+function all_values(obj) {
+  var keys = _keys(obj);
+  var length = keys.length;
+  var values = Array(length);
+  for (var i = 0; i < length; i++) {
+    values[i] = obj[keys[i]];
+  }
+  return values;
+}
+function _keys(obj) {
+  if (!isObject(obj)) return [];
+  if (Object.keys) return Object.keys(obj);
+  var keys = [];
+  for (var key in obj) if (_.has(obj, key)) keys.push(key);
+  return keys;
+}
+function isObject(obj) {
+  var type = typeof obj;
+  return type === "function" || (type === "object" && !!obj);
+}
+console.log(all_values({ red: "#FF0000", green: "#00FF00", white: "#FFFFFF" }));
