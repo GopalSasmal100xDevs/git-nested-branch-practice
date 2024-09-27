f38e7b0dcf4abcc6d62df2a5d2fa56a4b40a21e4
gopalprojectx100 <gopalsasmal100xdevs@gmail.com>
2024-09-27 10:30:03 +0530

    Dev- javascript code

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ function isObject(obj) {
   return type === "function" || (type === "object" && !!obj);
 }
 console.log(_keys({ red: "#FF0000", green: "#00FF00", white: "#FFFFFF" }));
+
+function FindAllMethods(obj) {
+  return Object.getOwnPropertyNames(obj).filter(function (property) {
+    return typeof obj[property] == "function";
+  });
+}
+//above code finds methods only
+//Following code finds both properties and methods
+console.log(FindAllMethods(Math));
+console.log(FindAllMethods(Array));
+function all_properties(obj) {
+  return Object.getOwnPropertyNames(obj);
+}
+
+console.log(all_properties(Math));
+
+console.log(all_properties(Array));
